37df403dd92d02d34f985a4425083eaf86cd96d6
kiekk <dbstnsgh2@naver.com>
2021-08-01 16:37:38 +0900

    create component TodoListItem
    TodoListItem 컴포넌트로 item 전달

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,19 +1,18 @@
 <template>
   <ul id="todolist">
-    <li
+    <TodoListItem
         v-for="item in list"
         :key="item.id"
-        :class="item.done ? 'checked' : ''"
-    >
-      <span>{{ item.todo }}</span>
-      <span v-if="item.done"> (완료)</span>
-      <span class="close">&#x00D7;</span>
-    </li>
+        :item="item"
+    />
   </ul>
 </template>
 
 <script>
+import TodoListItem from './TodoListItem'
+
 export default {
+  components: {TodoListItem},
   props: {
     list: {
       type: Array,
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li :class="item.done ? 'checked' : ''">
+    <span>{{ item.todo }}</span>
+    <span v-if="item.done"> (완료)</span>
+    <span class="close">&#x00D7;</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {
+          id: '',
+          todo: '',
+          done: false,
+        }
+      }
+    }
+  }
+}
+</script>
